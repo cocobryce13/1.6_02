87e52e43df6e5c5a2157e76f26ab3d6d533e8590
sokol <sokol@ASUS>
2024-07-16 21:36:01 +0500

    Добавьте файлы проекта.

---
+++ 1.6_02/1.6_02.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <windows.h>
+
+void param(int volue, int power, int result = 1)
+{
+    for (int i = 0; i < power; ++i)
+    {
+        result *= volue;
+    }
+    std::cout << volue << " в степени " << power << " = " << result;
+    std::cout << std::endl;
+}
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    param(5, 2);
+    param(3, 3);
+    param(4, 4);
+
+    return 0;
+}
